f3496a8a87a748f61cc3bb81790242cd5c49258b
Elaine Arbaugh <elaine.arbaugh@affirm.com>
2017-07-19 11:46:49 -0700

    Send rendered images in emails

--- cabot_alert_email/models.py
+++ cabot_alert_email/models.py
@@ -1,11 +1,13 @@
 from os import environ as env
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.template import Context, Template
+from urlparse import urljoin
 
 from cabot.cabotapp.alert import AlertPlugin
+from cabot.cabotapp.models.service import CALCULATED_FAILING_STATUS
 
 import requests
 import logging
@@ -25,6 +27,17 @@ class EmailAlert(AlertPlugin):
     name = "Email"
     author = "Jonathan Balls"
 
+    def _get_grafana_panel_image(self, panel_url, grafana_url):
+        url_partitions = panel_url.partition(grafana_url)
+        rendered_image_url = urljoin(urljoin(url_partitions[1], 'render'), url_partitions[2])
+
+        image_request = requests.get(rendered_image_url)
+        try:
+            image_request.raise_for_status()
+            return image_request.content
+        except requests.exceptions.HTTPError:
+            return None
+
     def send_alert(self, service, users, duty_officers):
         emails = [u.email for u in users if u.email] + [u.email for u in duty_officers if u.email]
         if not emails:
@@ -40,9 +53,23 @@ class EmailAlert(AlertPlugin):
         else:
             subject = 'Service back to normal: %s' % (service.name,)
         t = Template(email_template)
-        send_mail(
+
+        message = EmailMessage(
             subject=subject,
             message=t.render(c),
             from_email='Cabot <%s>' % env.get('CABOT_FROM_EMAIL'),
             recipient_list=emails,
         )
+
+        failing_metrics_checks = service.status_checks.filter(calculated_status=CALCULATED_FAILING_STATUS)\
+            .exclude(metricsstatuscheckbase__isnull=True)\
+            .filter(active=True)
+
+        for check in failing_metrics_checks:
+            if check.grafana_panel is not None:
+                message.attach('grafana.png',
+                               self._get_grafana_panel_image(check.grafana_panel.panel_url,
+                                                             check.grafana_panel.grafana_instance.url),
+                               'image/png')
+
+        message.send()
